b825e31d57bcd90365a35196065dfe0f2a267a09
pie-12 <tunglamnguyenxd@gmail.com>
2025-12-17 00:40:16 +0700

    feat(app): import user3 node app into /app

---
+++ app/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    // Lấy tên máy (Hostname) để biết code đang chạy trên container nào
+    const serverName = os.hostname();
+    res.send(`
+        <h1>Xin chào thầy và các bạn!</h1>
+        <p>Đây là Demo DevOps Project của Nhóm 3.</p>
+        <p>Code đang chạy trên Server: <b>${serverName}</b></p>
+        <p>Phiên bản: v1.0</p>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
