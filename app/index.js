b7e64fbea1f9a5fe641c27d6e9f018bd5584a057
duckwt1 <tranquangviet92nbk@gmail.com>
2025-12-18 22:46:11 +0700

    update2

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     const serverName = os.hostname();
     res.send(`
         <h1>Xin chào thầy và các bạn!</h1>
-        <p>Viet XINCHAO</p>
+        <p>Viet XINCHAO LAMNGOO</p>
         <p>Code đang chạy trên Server: <b>${serverName}</b></p>
         <p>Phiên bản: v1.0</p>
     `);
